e23f9626ef45583b23a4e711c31921580f24a779
Kemal070 <47315234+Kemal070@users.noreply.github.com>
2020-08-17 23:53:40 +0200

    UI update.
    Added flags and buttons,

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,141 @@ class SecondScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Learn Amazigh!")),
-      body: new Text('Hi'),
+      body: new Content(),
+    );
+  }
+}
+
+class Content extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: new EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 150.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: new GestureDetector(
+                  onTap: () {
+                    print("Dutch");
+                  },
+                  child: Image.asset(
+                    'assets/images/dutch_flag.jpg',
+                    height: 75,
+                    width: 75,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: new GestureDetector(
+                  onTap: () {
+                    print("English");
+                  },
+                  child: Image.asset(
+                    'assets/images/english_flag.jpg',
+                    height: 75,
+                    width: 75,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: new GestureDetector(
+                  onTap: () {
+                    print("Spanish");
+                  },
+                  child: Image.asset(
+                    'assets/images/spanish_flag.png',
+                    height: 75,
+                    width: 75,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: new GestureDetector(
+                  onTap: () {
+                    print("French");
+                  },
+                  child: Image.asset(
+                    'assets/images/french_flag.jpg',
+                    height: 75,
+                    width: 75,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: new GestureDetector(
+                  onTap: () {
+                    print("German");
+                  },
+                  child: Image.asset(
+                    'assets/images/german_flag.jpg',
+                    height: 75,
+                    width: 75,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        FlatButton(
+          color: Colors.lightBlue[900],
+          textColor: Colors.white,
+          disabledColor: Colors.grey,
+          disabledTextColor: Colors.black,
+          onPressed: () {
+            /*...*/
+          },
+          child: Text(
+            "Oefen",
+            style: TextStyle(fontSize: 20.0),
+          ),
+        ),
+        FlatButton(
+          color: Colors.yellow[800],
+          textColor: Colors.white,
+          disabledColor: Colors.grey,
+          disabledTextColor: Colors.black,
+          padding: EdgeInsets.all(8.0),
+          onPressed: () {
+            /*...*/
+          },
+          child: Text(
+            "Speel",
+            style: TextStyle(fontSize: 20.0),
+          ),
+        ),
+        FlatButton(
+          color: Colors.red[700],
+          textColor: Colors.white,
+          disabledColor: Colors.grey,
+          disabledTextColor: Colors.black,
+          padding: EdgeInsets.all(8.0),
+          onPressed: () {
+            /*...*/
+          },
+          child: Text(
+            "Score",
+            style: TextStyle(fontSize: 20.0),
+          ),
+        ),
+        FlatButton(
+          color: Colors.green,
+          textColor: Colors.white,
+          disabledColor: Colors.grey,
+          disabledTextColor: Colors.black,
+          padding: EdgeInsets.all(8.0),
+          onPressed: () {
+            /*...*/
+          },
+          child: Text(
+            "Over",
+            style: TextStyle(fontSize: 20.0),
+          ),
+        ),
+      ],
     );
   }
 }
